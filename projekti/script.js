809844819a76778bedba559f60c07484398a9749
Din0993 <fullfalkon@gmail.com>
2021-08-08 22:44:42 +0200

    Fixed sidebar

--- projekti/script.js
+++ projekti/script.js
@@ -3,16 +3,18 @@
 const sidebarMenu = document.querySelector(".navigation__menu__container");
 
 let activeSidebarKontakt = 0;
-
+const sidebarItems = document.querySelectorAll(".sidebar__item");
 sidebarMenu.addEventListener("click", function () {
   const sidebar = document.querySelector(".sidebar");
   if (activeSidebarKontakt === 0) {
     sidebar.style.opacity = "1";
     sidebar.style.width = "100%";
+    sidebarItems.forEach((item) => item.classList.remove("invisible"));
     activeSidebarKontakt = 1;
   } else {
     sidebar.style.opacity = "0";
     sidebar.style.width = "0";
+    sidebarItems.forEach((item) => item.classList.add("invisible"));
     activeSidebarKontakt = 0;
   }
 });
